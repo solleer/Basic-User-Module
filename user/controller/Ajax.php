6d8858123918da80fed8bed5d56ea8f4df3d4ecf
Richard <solleer@hotmail.com>
2017-03-30 15:27:12 -0400

    Change Ajax controller to use Request class instead of globals

--- user/controller/Ajax.php
+++ user/controller/Ajax.php
@@ -2,13 +2,15 @@
 namespace BasicUser\Controller;
 class Ajax {
     private $model;
+    private $request;
 
-    public function __construct(\User\Model\User $model) {
+    public function __construct(\User\Model\User $model, \Utils\Request $request) {
         $this->model = $model;
+        $this->request = $request;
     }
 
     public function user_exists() {
-        $username = $_POST['username'];
+        $username = $this->request->post('username');
         $user = $this->model->getUser($username);
         echo json_encode(empty($user));
         exit;
